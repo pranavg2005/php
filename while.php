9844d7376519a2228c94fa5e9027211e4a16e22e
pranavg2005 <108883084+pranavg2005@users.noreply.github.com>
2022-07-14 09:48:33 +0530

    while

---
+++ while.php
@@ -0,0 +1,21 @@
+<?php
+// day 2 variable declaration
+// day 3 for loop tables 
+// day 4 arrays and git 
+// day 5 while loop functions include require
+$a = 10;
+$i = 1;
+
+while($i<=$a) {  
+    echo $i*5;
+    echo "<br>\n";
+    $i++;
+}
+
+
+$cookies = 0; // variable declaration
+do {
+	echo "Mmmmm...I love cookies! *munch munch munch*";
+} while ($cookies > 1);
+
+?>
