f4cbbd67dc9f27f1f0a8e6d074a818f67cbb7ed3
pranavg2005 <108883084+pranavg2005@users.noreply.github.com>
2022-07-28 08:44:58 +0530

    html tables and php oops concepts

---
+++ access.php
@@ -0,0 +1,13 @@
+<?php
+class Fruit {
+  public $name;
+  protected $color;
+  private $weight;
+  
+}
+
+$mango = new Fruit();
+$mango->name = 'Mango'; // OK
+$mango->color = 'Yellow'; // ERROR
+$mango->weight = '300'; // ERROR
+?>
