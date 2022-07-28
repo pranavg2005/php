c279009c5c455d0133735771999c2c283a35ab7e
pranavg2005 <108883084+pranavg2005@users.noreply.github.com>
2022-07-28 08:47:58 +0530

    html tables and php oops concepts

---
+++ tables.php
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #1b1818;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: skyblue;
+}
+
+tr:nth-child(odd) {
+  background-color: rgb(161, 168, 161);
+}
+
+</style>
+</head>
+<body>
+
+<h2>HTML Tables</h2>
+
+<table>
+  <tr>
+    <td>Companys</th>
+    <td>Contact</th>
+    <td>Country</th>
+  </tr>
+  <tr>
+    <td>Dulance Tech</td>
+    <td>Deepak Yadav</td>
+    <td>India</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
+</body>
+</xml>
